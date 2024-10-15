0bfdde0518f725052e8190c4c17686cae1da3a43
Your Name <your.email@example.com>
2024-10-15 10:24:41 +0100

    function to get all the proposals

--- contracts/hello_world/src/lib.rs
+++ contracts/hello_world/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, Address, contractimpl, String, Env};
+use soroban_sdk::{contract, Address, contractimpl, String, Env, Vec};
 use crate::datatypes::{Proposal, VoterInfo, Datakey};
 
 
@@ -12,34 +12,40 @@ pub struct VotingContract;
 #[contractimpl]
 impl VotingContract {
 
+    // get proposal
+    pub fn get_proposal(env: Env, proposal_id: u32) -> Proposal {
+        env.storage().instance().get(&Datakey::Proposal(proposal_id)).unwrap_or_else(
+        || panic!("Proposal not found")
+        )
+    }
+
     // create proposal
     pub fn create_proposal(env: Env, title: String, description: String) -> u32 {
         let proposal_count: u32 = env.storage().instance().get(&Datakey::ProposalCount(0)).unwrap_or(0);
-        let mut _newCount: u32 = proposal_count + 1;  
+        
+        let mut _new_count: u32 = proposal_count + 1;  
 
         let proposal: datatypes::Proposal  = Proposal {
-            id: _newCount,
-            title: title,
-            description: description,
+            id: _new_count,
+            title,
+            description,
             vote_count: 0,
         };
 
 
         // set proposal
-        env.storage().instance().set(&datatypes::Datakey::Proposal(_newCount), &proposal);
+        env.storage().instance().set(&datatypes::Datakey::Proposal(_new_count), &proposal);
         // set proposal count
-        env.storage().instance().set(&datatypes::Datakey::ProposalCount(0), &_newCount);
+        env.storage().instance().set(&datatypes::Datakey::ProposalCount(0), &_new_count);
         
-        _newCount
+        _new_count
     }
 
 
     // vote proposal
     pub fn vote(env: Env, voter: Address, proposal_id: u32)  {
 
-        let mut proposal: Proposal = env.storage().instance().get(&Datakey::Proposal(proposal_id)).unwrap_or_else(
-        || panic!("Proposal not found")
-        );
+        let mut proposal: Proposal = Self::get_proposal(env.clone(), proposal_id);
 
         let mut voter_info: VoterInfo = env.storage().instance().get(&Datakey::VoterInfo(voter.clone())).unwrap_or(
             VoterInfo {
@@ -61,8 +67,25 @@ impl VotingContract {
        
         env.storage().instance().set(&Datakey::Proposal(proposal_id), &proposal);
         env.storage().instance().set(&Datakey::VoterInfo(voter.clone()), &voter_info);
+   
     }
 
+    // get all proposals
+    pub fn get_all_proposals(env: Env) -> Vec<Proposal> {
+        let proposal_count: u32 = env.storage().instance().get(&Datakey::ProposalCount(0)).unwrap_or(0);
+        
+        let mut proposals = Vec::new(&env);
+
+        for i in 1..proposal_count + 1 {
+            let proposal: Proposal = Self::get_proposal(env.clone(), i);
+            proposals.push_back(proposal);
+        }
+        
+        proposals
+    }
+
+
+    
 
 }
 
